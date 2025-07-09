837224387feab399dab3bfdaa3b20127cde1d419
Steve <steve@yarrumevets.com>
2025-07-09 09:29:47 -0700

    Delete no longer needed SAMPLE configs files

--- SAMPLE.aws.secret.js
+++
@@ -1,8 +0,0 @@
-const awsConfig = {
-  s3Bucket: "myshopname",
-  s3AccessKey: "XXXXXXXXXXXXXXXXXXXX",
-  s3SecretAccessKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-  region: "us-west-2",
-};
-
-export default awsConfig;
--- SAMPLE.shopify.secret.js
+++
@@ -1,7 +0,0 @@
-const shopifyConfig = {
-  adminApiAccessToken: "shpXX_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-  apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-  apiSecretKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-};
-
-export default shopifyConfig;
