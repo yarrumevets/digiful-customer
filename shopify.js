ccf57e960d2a0e77cf3e7839d8044a76c9b4ee2e
Steve <steve@yarrumevets.com>
2025-07-10 22:05:05 -0700

    fix - missing arrow on arrow func.

--- shopify.js
+++ shopify.js
@@ -3,7 +3,7 @@ import crypto from "crypto";
 
 // HMAC (Hash-based Message Authentication Code)
 // Verifies data integrity and authenticity using a shared secret key and a hash function.
-const verifyShopifyWebhook = (rawBody, reqHeaders) {
+const verifyShopifyWebhook = (rawBody, reqHeaders) => {
   const hmac = reqHeaders["x-shopify-hmac-sha256"];
   if (!hmac) {
     console.error("NO HMAC header.");
@@ -17,6 +17,6 @@ const verifyShopifyWebhook = (rawBody, reqHeaders) {
     Buffer.from(hmac, "base64"),
     Buffer.from(digest, "base64")
   );
-}
+};
 
 export { verifyShopifyWebhook };
