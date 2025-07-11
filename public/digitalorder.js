050b82cdddb3e2e751dd5a11a26b9bad000c7cf1
Steve <steve@yarrumevets.com>
2025-07-10 23:19:28 -0700

    fixed error.

--- public/digitalorder.js
+++ public/digitalorder.js
@@ -27,13 +27,13 @@ fetch(`/api/getsignedorderurls/${orderId}`)
   .then(({ products }) => {
     products.forEach((p) => {
       const li = document.createElement("li");
-      li.innerHTML = `
-        <h3>${p.title}</h3>
-        <ul>
-          <li>Size: ${createReadableDisplaySize(p.size)}</li>
-          <li>Version: ${p.version}.0</li>
-          <li><a href="/download/${p.code}" alt="${p.title}">Download</a></li>
-        </ul>`;
+      li.innerHTML = `<h3>${
+        p.title
+      }</h3><ul><li>Size: ${createReadableDisplaySize(
+        p.size
+      )}</li><li>Version: ${p.version}.0</li>
+      <li><a href="${p.url}" alt="${p.title}">Download</a></li></ul>`;
+      urlsList.appendChild(li);
     });
   })
   .catch((error) => {
